fea15bcd49c124b525de1321f7aea2731dbcaa92
Brandon Gulla <bgulla@palantir.com>
2025-05-22 13:28:18 -0400

    removing golang

--- golang/go-book-api/models/book.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Book struct {
-    ID            string `json:"id"`
-    Title         string `json:"title"`
-    Author        string `json:"author"`
-    PublishedYear int    `json:"published_year"`
-    AddedBy       string `json:"added_by"`
-}
